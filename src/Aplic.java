3f3222250edf88420c0519f95b8c2e8b670f2cd2
gustavohoc <goliveiradacruz@unomaha.edu>
2016-05-22 19:30:58 -0300

    Aplicacao criada

---
+++ src/Aplic.java
@@ -0,0 +1,12 @@
+import fatec.poo.model.*;
+
+public class Aplic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Cliente cli1 = new Cliente("40685124851","Gustavo Cruz",2500.00); 
+    }
+    
+}
